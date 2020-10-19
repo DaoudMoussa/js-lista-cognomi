18977ded910134448691c5685a08c1b11e417d58
Daoud <99m.daoud@gmail.com>
2020-10-19 16:48:55 +0200

    Aggiunto metodo con funzioni apposite

--- main.js
+++ main.js
@@ -25,29 +25,38 @@ userLastName = userLastName.charAt(0).toUpperCase() + userLastName.slice(1);
 // Inserimento cognome dell'utente nell'array
 lastNameList.push(userLastName);
 
-// SENZA FUNZIONI JS //
-var userLastNamePosition;
-while(lastNameList.length > sortedLastNameList.length) {
-    //Creazione variabile stringa con all'interno un carattere maggiore di tutte le lettere dell'alfabeto
-    var currentString = '{';
+// // SENZA FUNZIONI JS //
+// var userLastNamePosition;
+// while(lastNameList.length > sortedLastNameList.length) {
+//     //Creazione variabile stringa con all'interno un carattere maggiore di tutte le lettere dell'alfabeto
+//     var currentString = '{';
+//
+//     // ciclo che compara la currentString con tutte le stringe della lista e prende la piú piccola che non é gia presente nell'array ordinato
+//     for (var i = 0; i < lastNameList.length; i++) {
+//         // Se l'elemento dell'array in posizione i é piú piccolo di currentString e non é presente nell'array ordinato allora currentString diventa quell'elemento
+//         if(lastNameList[i] < currentString && !sortedLastNameList.includes(lastNameList[i])) {
+//             currentString = lastNameList[i];
+//         }
+//     }
+//     console.log(currentString);
+//     sortedLastNameList.push(currentString);
+//     // Se currentString é il cognome dell'utente allora il cognome dellútente l'ultimo elemento Inserito
+//     if(currentString == userLastName) {
+//         userLastNamePosition = sortedLastNameList.length - 1;
+//     }
+// }
+//
+// // Stampa array ordinato
+// console.log(sortedLastNameList);
+//
+// //Stampa posizione del cognome dellútente nell'array
+// console.log('Il cognome dell\'utente é nella posizione: ' + userLastNamePosition);
 
-    // ciclo che compara la currentString con tutte le stringe della lista e prende la piú piccola che non é gia presente nell'array ordinato
-    for (var i = 0; i < lastNameList.length; i++) {
-        // Se l'elemento dell'array in posizione i é piú piccolo di currentString e non é presente nell'array ordinato allora currentString diventa quell'elemento
-        if(lastNameList[i] < currentString && !sortedLastNameList.includes(lastNameList[i])) {
-            currentString = lastNameList[i];
-        }
-    }
-    console.log(currentString);
-    sortedLastNameList.push(currentString);
-    // Se currentString é il cognome dell'utente allora il cognome dellútente l'ultimo elemento Inserito
-    if(currentString == userLastName) {
-        userLastNamePosition = sortedLastNameList.length - 1;
-    }
-}
 
-// Stampa array ordinato
-console.log(sortedLastNameList);
 
-//Stampa posizione del cognome dellútente nell'array
-console.log('Il cognome dell\'utente é nella posizione: ' + userLastNamePosition);
+// CON FUNZIONI JS //
+lastNameList.sort();
+var userLastNamePosition = lastNameList.indexOf(userLastName);
+
+console.log('lista cognomi in ordine alfabetico: ' + lastNameList);
+console.log('posizione cognome utente numero: ' + userLastNamePosition);
